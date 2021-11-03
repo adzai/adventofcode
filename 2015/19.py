1d28875fc5631a353ac49cf3e445c54094306afc
Adam Zaiter <zaiter.adam7@gmail.com>
2021-11-03 16:47:16 +0100

    19 part 1

---
+++ 2015/19.py
@@ -0,0 +1,37 @@
+lines = []
+with open("19.txt") as f:
+    lines = list(map(lambda x: x.strip().split(), f.readlines()))
+
+# lines = [
+# "H => HO",
+# "H => OH",
+# "O => HH",
+# "",
+# ["HOHOHO"]]
+
+sequence = lines[-1][0]
+
+table = dict()
+for line in lines[:-2]:
+    key = line[0]
+    val = line[-1]
+    if table.get(key):
+        table[key] = table[key] + [val]
+    else:
+        table[key] = [val]
+
+def part1(sequence, table):
+    double_char = ""
+    result_table = dict()
+    for i, char in enumerate(sequence):
+        if i > 0:
+            double_char = sequence[i-1] + sequence[i]
+        if table.get(char):
+            for val in table[char]:
+                result_table[sequence[:i] + val + sequence[i+1:]] = None
+        if table.get(double_char):
+            for val in table[double_char]:
+                result_table[sequence[:i-1] + val + sequence[i+1:]] = None
+    return len(result_table.keys())
+
+print("Part 1:", part1(sequence, table))
