99b877ae092aef332f7250d01af6a9a2418c6dd3
Adam Zaiter <zaiter.adam7@gmail.com>
2021-12-05 19:40:24 +0100

    day 5

--- 2021/5.py
+++ 2021/5.py
@@ -15,25 +15,29 @@ class Line:
 
 def parse(input):
     lines = []
-    highest_x = 0
-    highest_y = 0
+    max_x = 0
+    max_y = 0
     for line in input:
         coords = line.split(" -> ")
         x1, y1 = map(int, coords[0].split(","))
         x2, y2 = map(int, coords[1].split(","))
         lines.append(Line(x1, x2, y1, y2))
-        highest_x = max(highest_x, x1, x2)
-        highest_y = max(highest_y, y1, y2)
+        max_x = max(max_x, x1, x2)
+        max_y = max(max_y, y1, y2)
+    return max_y, max_x, lines
+
+
+def create_grid(max_y, max_x):
     grid = []
-    for _ in range(highest_y + 1):
+    for _ in range(max_y + 1):
         row = []
-        for _ in range(highest_x + 1):
+        for _ in range(max_x + 1):
             row.append(".")
         grid.append(row)
-    return grid, lines
+    return grid
 
 
-def draw(grid, lines):
+def draw(grid, lines, part2=False):
     for line in lines:
         if line.x1 == line.x2:
             r = range(min(line.y1, line.y2), max(line.y1, line.y2) + 1)
@@ -50,7 +54,7 @@ def draw(grid, lines):
                     grid[line.y1][x] = 1
                 else:
                     grid[line.y1][x] += 1
-        else:
+        elif part2:
             step_x = -1 if line.x1 > line.x2 else 1
             step_y = -1 if line.y1 > line.y2 else 1
             rx = range(line.x1, line.x2 + step_x, step_x)
@@ -79,7 +83,53 @@ def count(grid):
     )
 
 
-grid, lines = parse(input)
-draw(grid, lines)
+def draw_fast(grid, lines):
+    count = 0
+    for line in lines:
+        if line.x1 == line.x2:
+            r = range(min(line.y1, line.y2), max(line.y1, line.y2) + 1)
+            for y in r:
+                key = (y, line.x1)
+                if grid.get(key) is None:
+                    grid[key] = 1
+                else:
+                    if grid[key] == 1:
+                        count += 1
+                    grid[key] += 1
+
+        elif line.y1 == line.y2:
+            r = range(min(line.x1, line.x2), max(line.x1, line.x2) + 1)
+            for x in r:
+                key = (line.y1, x)
+                if grid.get(key) is None:
+                    grid[key] = 1
+                else:
+                    if grid[key] == 1:
+                        count += 1
+                    grid[key] += 1
+        else:
+            step_x = -1 if line.x1 > line.x2 else 1
+            step_y = -1 if line.y1 > line.y2 else 1
+            rx = range(line.x1, line.x2 + step_x, step_x)
+            ry = range(line.y1, line.y2 + step_y, step_y)
+            for x, y in zip(rx, ry):
+                key = (y, x)
+                if grid.get(key) is None:
+                    grid[key] = 1
+                else:
+                    if grid[key] == 1:
+                        count += 1
+                    grid[key] += 1
+    return count
+
+
+max_y, max_x, lines = parse(input)
+# dict_grid = dict()
+# draw_fast(dict_grid, lines)
 # pretty_print(grid)
+grid = create_grid(max_y, max_x)
+draw(grid, lines)
+print("Part 1:", count(grid))
+grid = create_grid(max_y, max_x)
+draw(grid, lines, part2=True)
 print("Part 2:", count(grid))
