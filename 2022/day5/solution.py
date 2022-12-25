24c628061787784af95fd7886b51d8113117266e
adzai <zaiter.adam7@gmail.com>
2022-12-25 21:17:26 +0100

    day5

--- 2022/day5/solution.py
+++ 2022/day5/solution.py
@@ -5,29 +5,43 @@ with open("input.txt") as f:
     configuration = configuration.split("\n")[:-1]
     steps = steps.split("\n")[:-1]
 
-config_map = defaultdict(list)
-for line in configuration:
-    i = 0
-    while i < len(line):
-        char = line[i + 1].strip()
-        position = i // 4 + 1
-        if char != "":
-            config_map[position] = [char] + config_map[position]
-        i += 4
 
-for step in steps:
-    step = step.split()
-    num = int(step[1])
-    from_step = int(step[3])
-    to_step = int(step[5])
-    popped_vals = []
-    for i in range(num):
-        popped_vals.append(config_map[from_step].pop())
-        # config_map[to_step].append(config_map[from_step].pop())
-    config_map[to_step] += popped_vals[::-1]
+def init_config_map():
+    config_map = defaultdict(list)
+    for line in configuration:
+        i = 0
+        while i < len(line):
+            char = line[i + 1].strip()
+            position = i // 4 + 1
+            if char != "":
+                config_map[position] = [char] + config_map[position]
+            i += 4
 
-top_letters = ""
-for key in sorted(config_map.keys()):
-    top_letters += config_map[key][-1]
+    return config_map
 
-print(top_letters)
+
+def solve(steps, part1):
+    config_map = init_config_map()
+
+    for step in steps:
+        step = step.split()
+        num = int(step[1])
+        from_step = int(step[3])
+        to_step = int(step[5])
+        popped_vals = []
+        for _ in range(num):
+            if part1:
+                config_map[to_step].append(config_map[from_step].pop())
+            else:
+                popped_vals.append(config_map[from_step].pop())
+        config_map[to_step] += popped_vals[::-1]
+
+    top_letters = ""
+    for key in sorted(config_map.keys()):
+        top_letters += config_map[key][-1]
+
+    return top_letters
+
+
+print("Part 1:", solve(steps, part1=True))
+print("Part 2:", solve(steps, part1=False))
